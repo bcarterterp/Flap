54a10a35e337d1137fd7cb230ac803d485f785db
Caroline Wang <cxw320@case.edu>
2023-10-02 13:30:02 -0400

    add apiprovider and recipeRepositoryProvider to provider class

--- lib/presentation/providers/providers.dart
+++ lib/presentation/providers/providers.dart
@@ -1,11 +1,20 @@
 import 'package:equifax_app/data/repository/auth/auth_repository_impl.dart';
+import 'package:equifax_app/data/repository/recipe/recipe_repository_impl.dart';
+import 'package:equifax_app/data/source/network/spoonacular_api.dart';
 import 'package:equifax_app/domain/repository/auth/auth_repository.dart';
+import 'package:equifax_app/domain/repository/recipe/recipe_repository.dart';
 import 'package:equifax_app/presentation/screens/login/notifier/login_page_state.dart';
 import 'package:equifax_app/presentation/screens/login/notifier/login_page_state_notifier.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+final apiProvider = Provider<SpoonacularApi>((ref) => SpoonacularApiImpl());
+
 final authRepositoryProvider = Provider<AuthRepositoy>(
   (ref) => AuthRepositoyImpl(),
+);
+
+final recipeRepositoryProvider = Provider<RecipeRepository>(
+  (ref) => RecipeRepositoryImpl(api: ref.read(apiProvider),
 );
 
 final loginPageStateProvider =
