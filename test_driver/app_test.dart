ed4367a5e9305f734dfa541e670d29c9ac50e5f0
Mia Brunal <mia.brunal@willowtreeapps.com>
2023-10-03 10:02:31 -0400

    Add additional test as well as reorganize comments

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -1,5 +1,6 @@
 // Imports the Flutter Driver API.
 
+import 'package:flutter/material.dart';
 import 'package:flutter_driver/flutter_driver.dart';
 
 import 'package:test/test.dart';
@@ -7,10 +8,21 @@ import 'package:test/test.dart';
 void main() {
   group('Flutter driver test App', () {
     // First, you can define common Finders/variables and use them to locate widgets from the test suite
-
     final exampleFinder = find.byValueKey("example");
 
     var example_data = "example";
+
+    // We can hm,.ook into the button by using it's tooltip, which can be
+    // found by going to the widget inspector and clicking on
+    // the button in the tree, which will show where in the code
+    // it is defined:
+    SerializableFinder title = find.text("Flutter Demo Home Page");
+    SerializableFinder message =
+        find.text("You have pushed the button this many times:");
+    SerializableFinder initialCounterText = find.text("0");
+    SerializableFinder counterButton = find.byTooltip("Increment");
+
+    const duration = Duration(milliseconds: 500);
 
     late FlutterDriver driver;
 
@@ -20,7 +32,7 @@ void main() {
       driver = await FlutterDriver.connect();
     });
 
-// Close the connection to the driver after the tests have completed.
+    // Close the connection to the driver after the tests have completed.
 
     tearDownAll(() async {
       if (driver != null) {
@@ -29,15 +41,41 @@ void main() {
     });
 
     test("check health", () async {
+      // Based on given example in documentation
       Health health = await driver.checkHealth();
       print(health.status);
     });
 
     test('test page elements', () async {
-      SerializableFinder message = find.text("You have pushed the button this many times:");
-      await driver.waitFor(message);
+      // Based on given example in documentation
+      expect(await driver.getText(title), "Flutter Demo Home Page");
       expect(await driver.getText(message),
           "You have pushed the button this many times:");
+      expect(await driver.getText(initialCounterText), "0");
+      // There is no way to test for existence in flutter.
+      // This try catch waits for the element which is similar
+      // TO DO: Put this statement into a reusable custom function
+      try {
+        await driver.waitFor(counterButton, timeout: duration);
+        return true;
+      } catch (e) {
+        return false;
+      }
+    });
+
+    test('test increment button', () async {
+      // Custom example test. First we define our finders.
+      SerializableFinder afterCounterText = find.text("1");
+      // Next we await the element that needs to be interacted with
+      await driver.waitForTappable(counterButton);
+      // Now we ensure the counter is still at 0:
+      expect(await driver.getText(initialCounterText), "0");
+      // We now tap the FloatingActionButton
+      driver.tap(counterButton);
+      // We assert that the counter has incremented
+      expect(await driver.getText(afterCounterText), "1");
+      // Note: we do not need to use two finders if we have something else to hook into
+      // like a tooltip or unique type.
     });
   });
 }
