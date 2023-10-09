8636a932b3fe0acb91b3a94a5b37a17ca3fb9bd7
Mia Brunal <mia.brunal@willowtreeapps.com>
2023-10-09 15:33:22 -0400

    Remove weird return in home_widget_test

--- macos/widget_test/test/home_widget_test.dart
+++ macos/widget_test/test/home_widget_test.dart
@@ -8,7 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import '../screens/home_screen.dart';
-
 import 'package:flap_app/main.dart';
 
 void main() {
