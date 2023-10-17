38b3edab70f09a77982faf84a6c82ddf7e70c721
Caroline Wang <cxw320@case.edu>
2023-10-17 10:44:58 -0400

    add a cast to specify that the return type should be a Success class

--- test/presentation/screens/home/notifier/home_page_state_notifier_test.dart
+++ test/presentation/screens/home/notifier/home_page_state_notifier_test.dart
@@ -23,14 +23,15 @@ void main() {
   group('getRandomRecipes', () {
     test('returns success and updates HomePageState with recipeList', () async {
       const recipes = [Recipe(id: 1), Recipe(id: 2)];
-      final successResponse = Success<List<Recipe>, DioException>(recipes);
+      Success<List<Recipe>, DioException> successResponse =
+          Success<List<Recipe>, DioException>(recipes);
       //Without the below provideDummy, an error will be thrown because the generated mock class requires a dummy value(line 45 in the mocks.dart equivalent of this class)
       provideDummy<RequestResponse<List<Recipe>, DioException>>(
           successResponse);
 
       //Below is stubbed behavior for the getRandomRecipes call to return a successResponse
-      when(mockRecipeRepository.getRandomRecipes())
-          .thenAnswer((_) async => Future.value(successResponse));
+      when(mockRecipeRepository.getRandomRecipes()).thenAnswer((_) async =>
+          Future<Success<List<Recipe>, DioException>>.value(successResponse));
 
       // Call the getRandomRecipes method
       await notifier.getRandomRecipes();
