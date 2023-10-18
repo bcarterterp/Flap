3eac3db738a92152b6d2d515e3a58c5c3f7d72e6
Caroline Wang <cxw320@case.edu>
2023-10-18 16:15:51 -0400

    add flavorconfig

--- integration_test/login_test.dart
+++ integration_test/login_test.dart
@@ -1,4 +1,4 @@
-import 'package:flap_app/main.dart';
+import 'package:flap_app/main_common.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import '../test/presentation/screens/login/login_screen.dart';
--- lib/main_common.dart
+++ lib/main_common.dart
@@ -3,8 +3,12 @@ import 'package:flap_app/presentation/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(const MyApp());
+var flavorConfigProvider = StateProvider(
+  (ref) => FlavorConfig(appTitle: "Staging"),
+);
+
+void mainCommon() {
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -13,16 +17,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final config = context.read(flavorConfigProvider);
     const theme = AppTheme();
     // ProviderScope is what makes Riverpod work.
     // MaterialApp.router is what sets our routerConfig to our GoRouter object
-    return ProviderScope(
-      child: MaterialApp(
-        title: 'Flutter Boilerplate',
-        theme: theme.toThemeData(),
-        darkTheme: theme.toThemeDataDark(),
-        home: MaterialApp.router(routerConfig: navigationRouter),
-      ),
+    return MaterialApp(
+      title: config.state.appTitle,
+      theme: theme.toThemeData(),
+      darkTheme: theme.toThemeDataDark(),
+      home: MaterialApp.router(routerConfig: navigationRouter),
     );
   }
 }
