ead1e56b0b69617d5bac087ad30594f3bcdbe64c
Caroline Wang <cxw320@case.edu>
2023-10-19 16:55:05 -0400

    add dev and prod build flavors to android

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,6 @@ import 'package:flap_app/presentation/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-var flavorConfigProvider = StateProvider(
-  (ref) => FlavorConfig(appTitle: "Staging"),
-);
-
 void mainCommon() {
   runApp(const ProviderScope(child: MyApp()));
 }
@@ -17,12 +13,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final config = context.read(flavorConfigProvider);
     const theme = AppTheme();
     // ProviderScope is what makes Riverpod work.
     // MaterialApp.router is what sets our routerConfig to our GoRouter object
     return MaterialApp(
-      title: config.state.appTitle,
+      title: "Boiler plate",
       theme: theme.toThemeData(),
       darkTheme: theme.toThemeDataDark(),
       home: MaterialApp.router(routerConfig: navigationRouter),
