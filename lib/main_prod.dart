2f7d82f156cd03463faccfe8a5b1071d565f5b40
Caroline Wang <cxw320@case.edu>
2023-10-31 10:15:20 -0400

    initialize flavor with dev flavor in flavorconfig

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -4,6 +4,6 @@ import 'package:flap_app/util/flavor/flavor.dart';
 import 'main.dart' as runner;
 
 Future<void> main() async {
-  FlavorConfig.flavor = Flavor.dev;
+  FlavorConfig.flavor = Flavor.prod;
   await runner.main();
 }
--- lib/util/flavor/flavor_config.dart
+++ lib/util/flavor/flavor_config.dart
@@ -1,9 +1,10 @@
 import 'package:flap_app/util/flavor/flavor.dart';
 
 class FlavorConfig {
-  static Flavor? flavor;
+  //default flavor will be dev
+  static Flavor flavor = Flavor.dev;
 
-  static String get flavorName => flavor?.name ?? '';
+  static String get flavorName => flavor.name;
 
   static String get appTitle {
     switch (flavor) {
@@ -11,8 +12,6 @@ class FlavorConfig {
         return 'Flap Boilerplate (Dev Flavor)';
       case Flavor.prod:
         return 'Flap Boilerplate (Prod Flavor)';
-      default:
-        return 'Flap Boilerplate';
     }
   }
 
@@ -22,8 +21,6 @@ class FlavorConfig {
         return 'api.spoonacular.com'; //replace this with staging api host
       case Flavor.prod:
         return 'api.spoonacular.com'; //replace this with production api host
-      default:
-        return 'api.spoonacular.com';
     }
   }
 }
