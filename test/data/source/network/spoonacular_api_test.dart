c26039cbc92b3a8d0dfbffc5002f3f3bf7fd2cbc
Caroline Wang <cxw320@case.edu>
2023-10-31 11:13:48 -0400

    add test for checking if recipe list is returned

--- test/data/source/network/spoonacular_api_test.dart
+++ test/data/source/network/spoonacular_api_test.dart
@@ -1,26 +1,37 @@
 import 'package:dio/dio.dart';
+import 'package:flap_app/data/dto/recipe_dto.dart';
 import 'package:flap_app/data/source/network/spoonacular_api_impl.dart';
 import 'package:flap_app/domain/entity/recipe.dart';
 import 'package:flap_app/domain/entity/request_response.dart';
+import 'package:flap_app/domain/repository/flavor/flavor_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http_mock_adapter/http_mock_adapter.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
+import 'spoonacular_api_test.mocks.dart';
+
+@GenerateMocks([FlavorRepository])
 void main() {
   late Dio dio;
   late DioAdapter dioAdapter;
   late SpoonacularApiImpl spoonacularApi;
+  late MockFlavorRepository flavorRepo;
 
-  group('getRandomRecipes', () {
+  group('Spoonacular Api Unit Tests', () {
     setUp(() {
       dio = Dio(BaseOptions());
       dioAdapter = DioAdapter(dio: dio);
       dio.httpClientAdapter = dioAdapter;
-      spoonacularApi = SpoonacularApiImpl(dio: dio);
+      flavorRepo = MockFlavorRepository();
+      when(flavorRepo.getBaseUrlHost()).thenAnswer((realInvocation) => "");
+      spoonacularApi = SpoonacularApiImpl(dio: dio, flavorRepo: flavorRepo);
     });
 
     test(
-        'returns recipe list and a success request response object upon 200 code',
+        'Given the spoonacular api instance, when the api returns a 200 success code, a recipe list wrapped in a success request response will be returned',
         () async {
+      final recipeList = [const Recipe(id: 1)];
       dioAdapter.onGet(
         spoonacularApi.spoonacularUri.toString(),
         (server) => server.reply(
@@ -34,11 +45,19 @@ void main() {
         ),
       );
       final response = await spoonacularApi.getRandomRecipes();
+      final recipeListResponse = ((response as SuccessRequestResponse<List<Recipe>,DioException>).data)
+          .map((response) => response)
+          .toList();
+
       expect(
           response, isA<SuccessRequestResponse<List<Recipe>, DioException>>());
+      expect(
+          (response).data,
+          recipeListResponse);
     });
 
-    test('returns error request response object upon server error code 500',
+    test(
+        'Given the spoonacular api instance, when the api returns a 500 error code, an error response will be returned',
         () async {
       dioAdapter.onGet(
         "",
