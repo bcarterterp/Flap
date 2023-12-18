56003bae00f9e5d537001b8356e1c412c473d566
sagethompson99 <sagethompson85@gmail.com>
2023-12-18 17:45:06 -0500

    State for credit widgets, added repos for credit widget data, state for widget screen

--- lib/domain/entity/user_credit_history.dart
+++ lib/domain/entity/user_credit_history.dart
@@ -3,5 +3,12 @@ class UserCreditHistory {
     required this.data,
   });
 
-  final int data;
+  final List<CreditHistoryItem> data;
+}
+
+class CreditHistoryItem {
+  CreditHistoryItem({required this.date, required this.score});
+
+  final DateTime date;
+  final int score;
 }
