95c39c8bf9f4270184939ac099008384f8ff6339
Anthony Dade <145477058+AnthonyDadeWT@users.noreply.github.com>
2023-12-21 16:06:41 -0500

    Removing redefined package, adding crashlyitcs

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flap_app/firebase_options.dart';
 import 'package:flap_app/presentation/flap_app.dart';
 import 'package:flap_app/util/flavor/flavor.dart';
@@ -6,7 +7,7 @@ import 'package:flap_app/util/flavor/flavor_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main(List<String> arguments) {
+void main(List<String> arguments) async {
   const flavor = String.fromEnvironment('FLAVOR', defaultValue: 'dev');
   switch (flavor) {
     case 'prod':
@@ -23,8 +24,10 @@ void main(List<String> arguments) {
   runApp(const ProviderScope(child: FlapApp()));
 }
 
-void initalizeFirebase() async {
+Future<void> initalizeFirebase() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
 }
